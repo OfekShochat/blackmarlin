329dee80fb1c569bf4ca4fc5b7d7fb0cf244cc33
doruk <dsekercioglu2003@gmail.com>
2021-11-30 09:13:59 +0300

    Double extend conditions

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -317,6 +317,9 @@ pub fn search<Search: SearchType>(
                     local_context.reset_skip_move(ply);
                     if s_score < s_beta {
                         extension += 1;
+                        if !Search::PV && s_score + 75 < s_beta {
+                            extension += 1;
+                        }
                     } else if s_beta >= beta {
                         /*
                         Multi-cut:
